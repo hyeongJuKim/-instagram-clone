b1d67fcda2cb6cc287f33e0f0ee8556d48ac50ba
hj <gudwndhkd@nate.com>
2019-06-07 21:40:24 +0900

    react-create-app install

--- backend/settings.py
+++ backend/settings.py
@@ -75,7 +75,9 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend', 'build'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -86,6 +88,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend', 'build', 'static')
 ]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
