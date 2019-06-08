b179882f96de44ca58a469b3256566859fdd84d7
hj <gudwndhkd@nate.com>
2019-06-08 19:14:29 +0900

    nav 기본디자인, feed list 기본 디자인 완료

---
+++ frontend/src/components/main/FeedBoxList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './feedBox.css';
+
+class FeedBox extends Component {
+
+    render() {
+        return(
+            <div className="feed-box">
+                feed box<br/>
+                img<br/>
+            </div>
+        );
+    }
+}
+
+export default FeedBox;
